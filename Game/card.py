681c9029c540aa4e9c5192f25e31df38d503489b
Baldur Tryggvason <ballit85@gmail.com>
2018-05-11 20:27:31 +0000

    Initial commit

---
+++ Game/card.py
@@ -0,0 +1,26 @@
+class Card:
+
+    card_values = {
+        'Ace': 11,  # value of the ace is high until it needs to be low
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        '10': 10,
+        'Jack': 10,
+        'Queen': 10,
+        'King': 10
+    }
+
+    def __init__(self, suit, rank):
+        """
+        :param suit: The face of the card, e.g. Spade or Diamond
+        :param rank: The value of the card, e.g 3 or King
+        """
+        self.suit = suit.capitalize()
+        self.rank = rank
+        self.points = self.card_values[rank]
