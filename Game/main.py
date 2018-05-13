f3dd30eff7ee94cd6142dca5d90c19c19886b048
Baldur Tryggvason <ballit85@gmail.com>
2018-05-13 08:40:08 +0000

    Refactor

--- Game/main.py
+++ Game/main.py
@@ -5,13 +5,13 @@ from utils import get_percentage
 
 
 
-game = OverUnder(Human())
+game = OverUnder(Robot())
 game.play()
-# winner_list = []
-# total_rounds = 10000
-# for _ in range(total_rounds):
-#     winner_list.append(game.play())
-#     game.reset()
+winner_list = []
+total_rounds = 10000
+for _ in range(total_rounds):
+    winner_list.append(game.play())
+    game.reset()
 
-# dealer_wins = winner_list.count("Dealer")
-# print("Dealer won {0} games from {1} with a {2:.2f}% win percentage!".format(dealer_wins, total_rounds, get_percentage(total_rounds, dealer_wins)))
+dealer_wins = winner_list.count("Dealer")
+print("Dealer won {0} games from {1} with a {2:.2f}% win percentage!".format(dealer_wins, total_rounds, get_percentage(total_rounds, dealer_wins)))
